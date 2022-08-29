52a0cbf783bcf5624eee5859da290382fe40c1a0
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-29 14:54:12 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,25 +17,33 @@ public static void fatty_acids_saturated(String[] args):
 acid1.acid = "Lauric"
 acid1.structure = "CH3(CH2)10COOH"
 acid1.meltingPoint = 44
+cout << "The first saturated fatty acid is " + acid1.acid + "," + "with the element structure of" + acid1.structure + ", and with the melting point of " + acid1.meltingPoint + "." + "/n"
 acid2.acid = "Palmitic"
 acid2.structure = "CH3(CH2)14COOH"
 acid2.meltingPoint = 63
+cout << "The second saturated fatty acid is " + acid2.acid + "," + "with the element structure of" + acid2.structure + ", and with the melting point of " + acid2.meltingPoint + "." + "/n"
 acid3.acid = "Stearic"
 acid3.structure = "CH3(CH2)16COOH"
+acid3.meltingPoint = 70
+cout << "The third saturated fatty acid is " + acid3.acid + "," + "with the element structure of" + acid3.structure + ", and with the melting point of " + acid3.meltingPoint + "." + "/n"
 
 public static void fatty_acids_unsaturated(String[] args):
 acid4.acid = "Oleic"
 acid4.structure = "CH3(CH2)7CH=CH(CH2)7COOH"
 acid4.meltingPoint = 16
+cout << "The first unsaturated fatty acid is " + acid4.acid + "," + "with the element structure of" + acid4.structure + ", and with the melting point of " + acid4.meltingPoint + "." + "/n"
 acid5.acid = "Linoleic"
 acid5.structure = "CH3(CH2)4(CH=CHCH2)2(CH2)6COOH"
 acid5.meltingPoint = -5
+cout << "The second saturated fatty acid is " + acid5.acid + "," + "with the element structure of" + acid5.structure + ", and with the melting point of " + acid5.meltingPoint + "." + "/n"
 acid6.acid = "Linolenic"
 acid6.structure = "CH3CH2(CH=CHCH2)3(CH2)6COOH"
 acid6.meltingPoint = -11
+cout << "The first saturated fatty acid is " + acid6.acid + "," + "with the element structure of" + acid6.structure + ", and with the melting point of " + acid6.meltingPoint + "." + "/n"
 acid7.acid = "Arachidonic"
 acid7.structure = "CH3(CH2)4(CH=CHCH2)4(CH2)2COOH"
 acid.meltingPoint = -50
+cout << "The first saturated fatty acid is " + acid7.acid + "," + "with the element structure of" + acid7.structure + ", and with the melting point of " + acid7.meltingPoint + "." + "/n"
 
 class CommonFattyAcids{
     public:
@@ -51,43 +59,57 @@ public static void saturatedFats(String[] args):
 chemical1.chemical = "butter"
 chemical1.palmiticMP = 29
 chemical1.stearicMP = 9
+cout << "The first chemical is " + chemical1.chemical + "," + "with the palmitic melting point of" + chemical1.palmiticMP + ", and with the stearic melting point of " + chemical1.stearicMP + "." + "/n"
 chemical2.chemical = "lard"
 chemical2.palmiticMP = 30
 chemical2.stearicMP = 18
+cout << "The second chemical is " + chemical2.chemical + "," + "with the palmitic melting point of" + chemica2.palmiticMP + ", and with the stearic melting point of " + chemical2.stearicMP + "." + "/n"
 chemical3.chemical = "beef"
 chemical3.palmiticMP = 32
 chemical3.stearicMP = 25
+cout << "The third chemical is " + chemical3.chemical + "," + "with the palmitic melting point of" + chemical3.palmiticMP + ", and with the stearic melting point of " + chemical3.stearicMP + "." + "/n"
 chemical4.chemical = "corn oil"
 chemical4.palmiticMP = 10
 chemical4.stearicMP = 4
+cout << "The fourth chemical is " + chemical4.chemical + "," + "with the palmitic melting point of" + chemical4.palmiticMP + ", and with the stearic melting point of " + chemical4.stearicMP + "." + "/n"
 chemical5.chemical = "soybean"
 chemical5.palmiticMP = 7
 chemical5.stearicMP = 3
+cout << "The fifth chemical is " + chemical5.chemical + "," + "with the palmitic melting point of" + chemical5.palmiticMP + ", and with the stearic melting point of " + chemical5.stearicMP + "." + "/n"
 chemical6.chemical = "peanut"
 chemical6.palmiticMP = 7
 chemical6.stearicMP = 5
+cout << "The sixth chemical is " + chemical6.chemical + "," + "with the palmitic melting point of" + chemical6.palmiticMP + ", and with the stearic melting point of " + chemical6.stearicMP + "." + "/n"
 chemical7.chemical = "olive"
 chemical7.palmiticMP = 6
 chemical7.stearicMP = 4
+cout << "The seventh chemical is " + chemical7.chemical + "," + "with the palmitic melting point of" + chemical7.palmiticMP + ", and with the stearic melting point of " + chemical7.stearicMP + "." + "/n"
+
 public static void unsaturatedFats(String[] args):
 chemical1.oleicMP = 27
 chemical1.linoleicMP = 4
 chemical1.otherMP = 31
+cout << "The first chemical is " + chemical1.chemical + "," + "with the oleic melting point of" + chemical1.oleicMP + ",with the linoleic melting point of " + chemical1.linoleicMP + ", and " + "with the other melting point at" + chemical1.otherMP + "." + "/n"
 chemical2.oleicMP = 41
 chemical2.linoleicMP = 6
 chemical2.otherMP = 5
+cout << "The second chemical is " + chemical2.chemical + "," + "with the oleic melting point of" + chemical2.oleicMP + ",with the linoleic melting point of " + chemical2.linoleicMP + ", and " + "with the other melting point at" + chemical2.otherMP + "." + "/n"
 chemical3.oleicMP = 38
 chemical3.linoleicMP = 3
 chemical3.otherMP = 2
+cout << "The third chemical is " + chemical3.chemical + "," + "with the oleic melting point of" + chemical3.oleicMP + ",with the linoleic melting point of " + chemical3.linoleicMP + ", and " + "with the other melting point at" + chemical3.otherMP + "." + "/n"
 chemical4.oleicMP = 34
 chemical4.linoleicMP = 48
 chemical4.otherMP = 4
+cout << "The fourth chemical is " + chemical4.chemical + "," + "with the oleic melting point of" + chemical4.oleicMP + ",with the linoleic melting point of " + chemical4.linoleicMP + ", and " + "with the other melting point at" + chemical4.otherMP + "." + "/n"
 chemical5.oleicMP = 25
 chemical5.linoleicMP = 56
 chemical5.otherMP = 9
+cout << "The fifth chemical is " + chemical5.chemical + "," + "with the oleic melting point of" + chemical5.oleicMP + ",with the linoleic melting point of " + chemical5.linoleicMP + ", and " + "with the other melting point at" + chemical5.otherMP + "." + "/n"
 chemical6.oleicMP = 60
 chemical6.linoleicMP = 21
 chemical6.otherMP = 7
+cout << "The sixth chemical is " + chemical6.chemical + "," + "with the oleic melting point of" + chemical6.oleicMP + ",with the linoleic melting point of " + chemical6.linoleicMP + ", and " + "with the other melting point at" + chemical6.otherMP + "." + "/n"
 chemical7.oleicMP = 83
 chemical7.linoleicMP = 7
-
+cout << "The seventh chemical is " + chemical7.chemical + "," + "with the oleic melting point of" + chemical7.oleicMP + ", and with the linoleic melting point of " + chemical1.linoleicMP + "." + "/n"
